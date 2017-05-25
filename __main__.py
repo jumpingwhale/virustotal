b81c5228fc94d866573a4be97a9692b5e189076b
jumpingwhale <jumpingwhale@naver.com>
2017-05-25 13:17:51 +0900

    Add __main__.py for indepent package execution under cmd environment

---
+++ __main__.py
@@ -0,0 +1,38 @@
+"""
+패키지 단독 실행시 cmd 로 쿼리를 할 수 있다
+e.g.) python -m virustotal MYAPIKEY 0cdf55626e56ffbf1b198beb4f6ed559
+"""
+
+from .connections import Connection
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description='Retrieve VirusTotal scan report')
+parser.add_argument('apikey', type=str, help='your virustotal api key')
+parser.add_argument('hash', metavar='MD5', type=str, nargs='+', help='MD5, SHA1, SHA256 hash values to query')
+parser.add_argument('-o', '--out', type=str, help='specify to save result as file')
+
+args = parser.parse_args()
+
+key = [args.apikey]
+hashes = args.hash
+outFile = args.out
+
+conn = Connection(key, private=False)
+
+result = ''
+for hash in hashes:
+    tmp = json.dumps(conn.scan(hash), indent=2)
+    title = '[%s]' % hash
+    print(tmp)
+    result += title + '\n' + tmp + '\n'
+
+if outFile is not None:
+    with open(outFile, 'w') as out:
+        out.write(result)
+
+
+
+
+
+
